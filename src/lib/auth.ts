6ed6e92854ccba1aa397ea7634b7783a56fee731
devilal0214 <devilal0214@gmail.com>
2026-06-18 20:41:19 +0530

    Updated dependency on vite packeges

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,6 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     signIn: '/login',
     error: '/login',
   },
+  trustHost: true,
   providers: [
     Credentials({
       name: 'credentials',
